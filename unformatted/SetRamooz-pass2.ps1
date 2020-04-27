e3abdf91e2abcc624f967d9695cc37fc30955b67
Lateef Shaikh <lateef_sagar@yahoo.com>
2020-04-27 10:57:43 -0400

    Completed till 39

--- unformatted/SetRamooz-pass2.ps1
+++ unformatted/SetRamooz-pass2.ps1
@@ -14,7 +14,7 @@ foreach($oldFile in $arr) {
     #Write-Output $text
 
     $text -replace '[٠-٩]+.\s','<span data-type="eoa-sc" class="eoAaya">۝$&</span>\r\n' `
-    -replace '(.+?)(\n|$)+','<span data-type="aaya" data-aaya="16" data-juz="1" data-ruku="1">$1</span>\r\n' `
+    -replace '(.+?)(\n|$)+','<span data-type="aaya" data-aaya="1" data-juz="1" data-ruku="1">$1</span>\r\n' `
     -replace '<span data-type="aaya" data-aaya="16" data-juz="1" data-ruku="1"></span>','' `
     -replace '<span data-type="aaya" data-aaya="16" data-juz="1" data-ruku="1">\r\n</span>','' `
     -replace '\s<span','\r\n<span' `
